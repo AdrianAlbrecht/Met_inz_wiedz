9b20b21e4d001488711a4887df04887fee8afbe3
AdrianAlbrecht <155376@student.uwm.edu.pl>
2022-03-29 21:03:15 +0200

    nauczyciel lab_4

--- Lab_03/lab3.py
+++ Lab_03/lab3.py
@@ -92,10 +92,25 @@ def metryka_euklidesowa_skalar(l1, l2):
 
 # - kolorki
 # petla:
-# - srodek masy
+# - srodek masy 
 # - nowe kolorki względem srodka masy
 # - konczymy petle, gdy kolorki sie nie zmieniaja
 ##### DO DOMU DOKOŃCZYĆ ######
+def srodek_masy(indeksy:list, lista:list):
+    odleglosci = []
+    temp_odl = 0
+    for ele in indeksy:
+        for ele_por in indeksy:
+            temp_odl+=metryka_euklidesowa(lista[ele], lista[ele_por])
+        odleglosci.append(temp_odl)
+        temp_odl = 0
+    mini = 0
+    for i in range(1,len(odleglosci)):
+        if odleglosci[mini]>odleglosci[i] :
+            mini = i
+    return mini
+    
+
 def nauczyciel(lista):
     grupy = dict()
     for ele in lista:
@@ -105,17 +120,71 @@ def nauczyciel(lista):
         else:
             grupy[decyzyjna]=[ele]
     matrix_without_decission = [l[:14]+[float(randrange(len(grupy.keys())))] for l in lista]
-    grupy = dict()
-    for ele in matrix_without_decission:
-        decyzyjna = ele[14]
-        if decyzyjna in grupy.keys():
-            grupy[decyzyjna].append(ele)
-        else:
-            grupy[decyzyjna]=[ele]
     
-nauczyciel(matrix)
+    zmiany = True
+    while(zmiany):
+        zmiany = False
+        grupy = dict()
+        for i in range(len(matrix_without_decission)):
+            decyzyjna = matrix_without_decission[i][-1]
+            if decyzyjna in grupy.keys():
+                grupy[decyzyjna].append(i)
+            else:
+                grupy[decyzyjna]=[i]
+        new_matrix = []
+        for ele in grupy.values():
+            new_matrix+=ele
+        srodki = []
+        for lista in grupy.values():
+            srodki.append(lista[srodek_masy(lista, matrix_without_decission)])
+        odleglosci = []
+        for ele in new_matrix:
+            for sr in srodki:
+                odleglosci.append(metryka_euklidesowa(matrix_without_decission[ele], matrix_without_decission[sr]))
+            mini = 0
+            ile = 1
+            for i in range(1,len(odleglosci)):
+                if odleglosci[mini]>odleglosci[i] :
+                    mini = i
+                    ile = 1
+                elif odleglosci[mini]==odleglosci[i] :
+                    ile=ile+1
+            if ile==1 :
+                if matrix_without_decission[ele][-1]!=matrix_without_decission[srodki[mini]][-1] :
+                    matrix_without_decission[ele][-1]=matrix_without_decission[srodki[mini]][-1]
+                    zmiany = True
+            elif ile>1:
+                matrix_without_decission[ele][-1]= None
+                zmiany = True
+            odleglosci=[]
+    # for sr in srodki:
+    #     print(matrix_without_decission[sr])
+    return matrix_without_decission
+        
+
+def matrix_difference(m1,m2):
+    ile = 0
+    for i in range(len(m1)):
+        if(m1[i]!=m2[i]):
+            ile=ile+1
+    return str((len(m1)-ile)/len(m1)*100)+" % pokrycia w stosunku do oryginału"
+    
+    
+new_matrix = nauczyciel(matrix)
+#print(new_matrix)
+grupy = dict()
+for ele in new_matrix:
+      decyzyjna = ele[14]
+      if decyzyjna in grupy.keys():
+          grupy[decyzyjna].append(ele)
+      else:
+          grupy[decyzyjna]=[ele]
+for key in grupy.keys():
+    print("{0}: {1}".format(key,len(grupy[key])))
+print(matrix_difference(matrix, new_matrix))
+
 
 
 ########################## DO DOMU METODA MONTE CARLO DLA FUNKCJI (CAŁKOWANIE) ORAZ METODA PROSTOKĄTÓW/TRAPEZÓW ################################
 #### y=x ####
-#### y=sin ####    
+#### y=sin ####
